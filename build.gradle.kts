5a5d4c2ef03757d59528d9e21a800aeb1bd14236
jeany0515 <devxu@thoughtworks.com>
2024-08-01 19:09:04 +0800

    add the retrofit dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,12 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Retrofit dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
